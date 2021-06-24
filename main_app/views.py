672ae5c73c2174e1a0e21e9edb80ca23467398eb
Jordan Wright <81875454+behemoth0132@users.noreply.github.com>
2021-06-24 06:39:48 -0700

    second commmit

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 # main_app/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
 
 def index(request):
   return render(request, 'index.html')
@@ -9,17 +10,9 @@ def about(request):
   return render(request, 'about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+def cats_show(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/show.html', {'cat': cat})
