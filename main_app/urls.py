cb2eea37be41ea99b67d6dc774ca12f83c02efa4
Jordan Wright <81875454+behemoth0132@users.noreply.github.com>
2021-06-24 01:23:20 -0700

    first commit

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+# main_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+ path('', views.index, name='index'),
+ path('about/', views.about, name='about'),
+ path('cats/', views.cats_index, name='cats_index')
+]
